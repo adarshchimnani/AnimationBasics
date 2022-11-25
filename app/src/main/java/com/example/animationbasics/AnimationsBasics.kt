f59a5a667d8e7e2ac58502a5476c21b487da1d78
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-25 12:47:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/animationbasics/AnimationsBasics.kt
@@ -0,0 +1,13 @@
+package com.example.animationbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimationsBasics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
