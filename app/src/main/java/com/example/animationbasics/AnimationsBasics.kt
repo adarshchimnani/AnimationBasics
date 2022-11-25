313570a3aaf14cd1c74365cc3808373d1f2c1f4e
adarshchimnani <67822194+adarshchimnani@users.noreply.github.com>
2022-11-25 12:53:07 +0500

    Initialized variable for image and toggle Button in the main activity

--- app/src/main/java/com/example/animationbasics/AnimationsBasics.kt
+++ app/src/main/java/com/example/animationbasics/AnimationsBasics.kt
@@ -1,13 +1,22 @@
 package com.example.animationbasics
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ToggleButton
 
 class AnimationsBasics : AppCompatActivity() {
+
+    lateinit var image: ImageView
+    lateinit var togBtn: ToggleButton
+    lateinit var anim: ObjectAnimator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        image = findViewById(R.id.image)
+        togBtn = findViewById(R.id.toggleBtn)
     }
 }
